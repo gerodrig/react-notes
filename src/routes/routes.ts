0a7fa03fd5b914aa962b7c694528c9928f5a3a24
Gerardo Rodriguez <garc@outlook.com>
2022-01-07 17:13:47 -0500

    Basic Lazyload Ready

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,8 +13,8 @@ interface Route {
 //component to be loaded under demand
 
 const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage1'));
-const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage1'));
+const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2'));
+const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3'));
 
 export const routes: Route[] = [
 	{
